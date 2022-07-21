c666b619d7024727fbc5de11b01a25fe551da137
Brompy <bromplicated@gmail.com>
2022-07-21 13:33:42 +0700

    added API key to header

--- Here's a Cat/main.js
+++ Here's a Cat/main.js
@@ -1,9 +1,11 @@
+
+
 document.querySelector('button').addEventListener('click', getCat)
 
 function getCat() {
     const url = 'https://api.thecatapi.com/v1/images/search'
     
-    fetch(url)
+    fetch(url, {headers: {'x-api-key':'bd5efee3-8629-4693-a772-d8c5fe8586e4'}})
     .then(res => res.json())
     .then(data => {
         console.log(data)
