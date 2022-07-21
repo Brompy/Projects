f026aa8391ae6cfe2b9b6596c11dc2a25990cb25
Brompy <bromplicated@gmail.com>
2022-07-21 17:14:16 +0700

    populated dropdown menu with list of all breeds

--- Here's a Cat/main.js
+++ Here's a Cat/main.js
@@ -1,15 +1,28 @@
+const BREEDS_URL = 'https://api.thecatapi.com/v1/breeds'
 
+const select = document.querySelector('.breeds')
 
-document.querySelector('button').addEventListener('click', getCat)
-
-function getCat() {
-    const url = 'https://api.thecatapi.com/v1/images/search'
-    
-    fetch(url, {headers: {'x-api-key':'bd5efee3-8629-4693-a772-d8c5fe8586e4'}})
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        document.querySelector('img').src = data[0].url;
+fetch(BREEDS_URL, {headers: {'x-api-key':'bd5efee3-8629-4693-a772-d8c5fe8586e4'}}) 
+    .then(res => {
+        return res.json();
     })
-    .catch(console.error)
-}
+    .then(data => {
+        for(let i=0; i < data.length; i++) {
+            const option = document.createElement('option')
+            option.value = data[i].name;
+            option.innerText = data[i].name;
+            select.appendChild(option)
+        }
+    })
+        
+        
+
+
+//     fetch(url, {headers: {'x-api-key':'bd5efee3-8629-4693-a772-d8c5fe8586e4'}})
+//     .then(res => res.json())
+//     .then(data => {
+//         console.log(data)
+//         document.querySelector('img').src = data[0].url;
+//     })
+//     .catch(console.error)
+// }
