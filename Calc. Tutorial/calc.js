ca8def7446e9bdf66e696aba78a7f1b02ca9c61c
Brompy <bromplicated@gmail.com>
2022-09-10 12:50:35 +0700

    add waitingForSecondOperator flag to updateDisplay

--- Calc. Tutorial/calc.js
+++ Calc. Tutorial/calc.js
@@ -6,9 +6,14 @@ const calculator = {
 };
 
 function inputDigit(digit) {
-    const { displayValue } = calculator;
+    const { displayValue, waitingForSecondOperand } = calculator;
     //Overwrite 'displayValue' if the current value is '0', otherwise append to it.
-    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+    if(waitingForSecondOperand === true) {
+        calculator.displayValue = digit;
+        calculator.waitingForSecondOperand = false;
+    } else {
+        calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+    }
     console.log(calculator);
 }
 
