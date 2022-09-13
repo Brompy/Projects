e196668d9370768d52f8efe3def09b51fe7371ab
Brompy <bromplicated@gmail.com>
2022-09-13 18:55:48 +0700

    refactored event listener

--- Calc. Tutorial/calc.js
+++ Calc. Tutorial/calc.js
@@ -20,6 +20,11 @@ function inputDigit(digit) {
 //
 
 function inputDecimal(dot) {
+    if (calculator.waitingForSecondOperand === true) {
+        calculator.displayValue = '0.';
+        calculator.waitingForSecondOperand = false;
+        return
+    }
     //IF the 'displayValue' property does not already contain a decimal point...
     if (!calculator.displayValue.includes(dot)) {
         //append the decimal point
@@ -86,29 +91,30 @@ updateDisplay();
 const keys = document.querySelector('.calculator-keys');
 keys.addEventListener('click', (event) => {
     const { target } = event;
-
+    const { value } = target;
     if(!target.matches('button')) {
         return;
     }
 
-    if(target.classList.contains('operator')) {
-        handleOperator(target.value);
-        updateDisplay();
-        return;
+    switch (value) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '=':
+            handleOperator(value);
+            break;
+        case '.':
+            inputDecimal(value);
+            break;
+        case 'all-clear':
+            resetCalculator();
+            break;
+        default:
+            //check if it's an integer
+            if(Number.isInteger(parseFloat(value))) {
+                inputDigit(value);
+            }
     }
-
-    if(target.classList.contains('decimal')) {
-        inputDecimal(target.value);
-        updateDisplay();
-        return;
-    }
-
-    if(target.classList.contains('all-clear')) {
-        resetCalculator();
-        updateDisplay();
-        return;
-    }
-
-    inputDigit(target.value);
     updateDisplay();
 });
