81d0d779320a880848a7a798e2be43af65a31fda
Brompy <bromplicated@gmail.com>
2023-05-07 11:25:01 +0700

    I just need to START OVER

--- Snooker Keeper/Snooker.js
+++ Snooker Keeper/Snooker.js
@@ -9,51 +9,6 @@ const scoreValues = {
   foul: -7,
  };
 
- const balls = document.querySelectorAll('.ball');
- balls.forEach(ball => { 
-  ball.addEventListener('click', handleClick);
-  })
-
- function handleClick(event) { 
-  const element = event.target;
-  const playerScoreElement = element.parentElement.querySelector('.playerScore h2');
-  console.log(playerScoreElement);
-
-  if (!playerScoreElement) {  
-    return;
-  }
-
-  let playerScore = Number(playerScoreElement.textContent.split(' ')[2]);
-
-  if(element.classList.contains('ball')) { 
-    const ballColor = element.classList.item(1);
-    const ballScore = getBallScore(ballColor);
-    playerScore += ballScore;
-   } else if (element.classList.contains('foul')) { 
-    playerScore += handleFoul(playerScore);
-    }
-
-    updatePlayerScore(playerScoreElement, playerScore)
-  }
-
-  function getBallScore(ballColor) {
-    return scoreValues[ballColor];
-  }
-
-  function handleFoul(playerScore) { 
-    const foulScore = scoreValues.foul;
-    playerScore -= foulScore;
-    if (playerScore < 0) { 
-      playerScore = 0;
-     }
-     return playerScore;
-  }
-
-   function updatePlayerScore(playerScoreElement, playerScore) {
-    playerScoreElement.textContent = `Player 1 Score: ${playerScore}`;
-   }
-
-   console.log(scoreValues);
 
 
 
